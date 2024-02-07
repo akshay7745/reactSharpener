a07860a7e42f22f0fd2df071ca268d2a52fbf718
akshay7745 <akshay7745@gmail.com>
2024-02-07 13:52:32 +0530

    Added change amount button

--- 01-starting-setup/src/components/Expense/ExpenseItem.js
+++ 01-starting-setup/src/components/Expense/ExpenseItem.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 const ExpenseItem = (props) => {
-  const { date, location, title, amount } = props;
+  const [title, setTitle] = useState(props.title);
+  const [amt, setAmt] = useState(props.amount);
+  const { date, location, title1, amount } = props;
   const buttonHandler = (e) => {
     // Removing expenseItem via dom manipulation
-    e.target.parentElement.parentElement.removeChild(e.target.parentElement);
-    console.log(`Button Clicked!!!`);
+    // e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+    setTitle("updated!!!");
+    console.log(title);
+  };
+  const amountButtonHandler = (e) => {
+    setAmt(100);
   };
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
-      <ExpenseDetails location={location} title={title} amount={amount} />
+      <ExpenseDetails location={location} title={title} amount={amt} />
       <button type="button" onClick={buttonHandler}>
-        Click
+        Change title
+      </button>
+      <button type="button" onClick={amountButtonHandler}>
+        Change amount
       </button>
     </div>
   );
