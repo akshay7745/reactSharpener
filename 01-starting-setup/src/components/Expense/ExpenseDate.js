03b92390008c896d96a32009c3ece135ff066757
akshay7745 <akshay7745@gmail.com>
2024-02-07 12:11:07 +0530

    Added event listener to button in react and removed expense list via dom manipulation

--- 01-starting-setup/src/components/Expense/ExpenseDate.js
+++ 01-starting-setup/src/components/Expense/ExpenseDate.js
@@ -2,26 +2,26 @@ import React from "react";
 
 const ExpenseDate = (props) => {
   const { date } = props;
-  //   return (
-  //     <div>
-  //       <div>{date.toLocaleString("en-US", { day: "2-digit" })}</div>
-  //       <div>{date.toLocaleString("en-US", { month: "long" })}</div>
-  //       <div>{date.getFullYear()}</div>
-  //     </div>
-  //   );
-  return React.createElement("div", {}, [
-    React.createElement(
-      "div",
-      {},
-      date.toLocaleString("en-US", { day: "2-digit" })
-    ),
-    React.createElement(
-      "div",
-      {},
-      date.toLocaleString("en-US", { month: "long" })
-    ),
-    React.createElement("div", {}, date.getFullYear()),
-  ]);
+  return (
+    <div>
+      <div>{date.toLocaleString("en-US", { day: "2-digit" })}</div>
+      <div>{date.toLocaleString("en-US", { month: "long" })}</div>
+      <div>{date.getFullYear()}</div>
+    </div>
+  );
+  // return React.createElement("div", {}, [
+  //   React.createElement(
+  //     "div",
+  //     {},
+  //     date.toLocaleString("en-US", { day: "2-digit" })
+  //   ),
+  //   React.createElement(
+  //     "div",
+  //     {},
+  //     date.toLocaleString("en-US", { month: "long" })
+  //   ),
+  //   React.createElement("div", {}, date.getFullYear()),
+  // ]);
 };
 
 export default ExpenseDate;
--- 01-starting-setup/src/components/Expense/ExpenseItem.js
+++ 01-starting-setup/src/components/Expense/ExpenseItem.js
@@ -5,22 +5,29 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 const ExpenseItem = (props) => {
   const { date, location, title, amount } = props;
+  const buttonHandler = (e) => {
+    // Removing expenseItem via dom manipulation
+    e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+    console.log(`Button Clicked!!!`);
+  };
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={date} />
+      <ExpenseDetails location={location} title={title} amount={amount} />
+      <button type="button" onClick={buttonHandler}>
+        Click
+      </button>
+    </div>
+  );
 
-  // return (
-  //   <div className="expense-item">
-  //     <ExpenseDate date={date} />
-  //     <ExpenseDetails location={location} title={title} amount={amount} />
-  //   </div>
-  // );
-
-  return React.createElement("div", { className: "expense-item" }, [
-    React.createElement(ExpenseDate, { date: date }),
-    React.createElement(ExpenseDetails, {
-      title: title,
-      location: location,
-      amount: amount,
-    }),
-  ]);
+  // return React.createElement("div", { className: "expense-item" }, [
+  //   React.createElement(ExpenseDate, { date: date }),
+  //   React.createElement(ExpenseDetails, {
+  //     title: title,
+  //     location: location,
+  //     amount: amount,
+  //   }),
+  // ]);
 };
 
 export default ExpenseItem;
